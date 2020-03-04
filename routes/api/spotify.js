df212419eb2347dab2515ef46d4fcd6f0caf9bdb
David Murphy <djmgeneseo@gmail.com>
2020-03-04 00:18:23 -0500

    add config and register app with spotify

--- routes/api/spotify.js
+++ routes/api/spotify.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const Axios = require('axios');
+const axios = require('axios');
 
 // 'api/spotify'
 
 // GET https://api.spotify.com/v1/audio-analysis/{id}
 router.get('/audio-analysis/:SONG_ID', async (req, res) => {
-    // TODO: AXIOS REQUEST TO SPOTIFY (create axios HOF)
+    axios.get(
+        `https://api.spotify.com/v1/audio-analysis/${req.params.SONG_ID}`
+    ).then(spotifyRes => {
+        spotifyRes.
+    })
 })
